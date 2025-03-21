4a6fc408d99c9c30438012db4e5d4b5a5d2f08c8
SebastianSosa <s.sosa@live.fr>
2025-03-21 16:15:26 +0100

    NBDA runing!!!!
    library(BI)
    library(reticulate)
    library(jsonlite)
    # setup platform------------------------------------------------
    m=importBI(platform='cpu')
    pd <- import("pandas")
    covariates_data <- fromJSON("C:/Users/SSosa/AppData/Local/R/win-library/4.3/BI/data/NBDA_data.json")
    names(covariates_data)
    for(a in 1:length(covariates_data)){
      print(dim(covariates_data[[a]]))
      covariates_data[[a]] = jnp$array(covariates_data[[a]])
    }
    
    covDF_slice <- covariates_data[['covDF']][
      reticulate:::py_slice(NULL, NULL, NULL),  # equivalent to ":" in Python for the first dimension
      reticulate:::py_slice(NULL, NULL, NULL),  # equivalent to ":" in Python for the second dimension
      0                           # selecting the first slice along the third dimension (Python uses 0-based indexing)
    ]
    
    nbda = m$nbda(covariates_data$network, covariates_data$status)
    nbda$import_covDV(covariates_data$covDV)
    nbda$import_covDF(covDF_slice)
    nbda$import_covNV(covariates_data$covV$reshape(to.jnp.shape(c(-1, 200,100))))
    nbda$import_covNF(pd$DataFrame(covariates_data$covF))
    
    cat(nbda$print_model())
    
    m$data_on_model = nbda$get_cov()
    m$run(nbda$model)

--- R/to.jnp.shape.R
+++ R/to.jnp.shape.R
@@ -1,4 +1,4 @@
 to.jnp.shape <- function(shape){
   # COnvert R shape to compatiple python jno shape
-  shape=do.call(tuple, as.list(as.integer(shape)))
+  do.call(tuple, as.list(as.integer(shape)))
 }
