7e021c0c01b0df8647e875e7ab9e830f980a0304
Sosa Sebastian <s.sosa@live.fr>
2025-10-29 09:22:35 +0100

    Remove redundant backend parameter in importBI.R

--- R/importBI.R
+++ R/importBI.R
@@ -61,8 +61,7 @@ importBI <- function(
                                       cores = reticulate::r_to_py(cores),
                                       rand_seed = reticulate::r_to_py(rand_seed),
                                       deallocate = reticulate::r_to_py(deallocate),
-                                      print_devices_found =  reticulate::r_to_py(print_devices_found), 
-                                      backend = reticulate::r_to_py(backend),
+                                      print_devices_found =  reticulate::r_to_py(print_devices_found)
                                       )
   
   # A helper Python function if needed
